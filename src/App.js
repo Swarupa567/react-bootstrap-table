641c80626c63b057f77f748e8df2ed99624340cc
Swarupa567 <swarupachowdaryp@gmail.com>
2022-10-25 08:03:37 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      {/* <ul className="list"> */}
+       {/* {data.filter(user => user.ResponseDesc.toLowerCase().includes(query)).map((user,index) =>(
+         <li key ={index} className="listItem">{user.superInstId}</li>
+       ))} */}
+      {/* </ul> */}
+       <Table />
+
     </div>
   );
 }
